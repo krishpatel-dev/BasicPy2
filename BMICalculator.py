83d3caf1c9173539a73c45b7499ae14dc318539a
Krish Patel <krishpatel2404@gmail.com>
2025-06-26 23:54:48 +0530

    commit

---
+++ BMICalculator.py
@@ -0,0 +1,11 @@
+height = input()
+
+weight = input()
+
+height1 = float(height)
+weight1 = int(weight)
+bmi = weight1 / (height1*height1)
+
+bmi1 = int(bmi)
+
+print(bmi1)
---
+++ LifeInWeeks.py
@@ -0,0 +1,6 @@
+age = input()
+
+year = int(age)*52
+week = 4680-int(year)
+
+print(f"You have {week} weeks left.")
